32e986604372fe5400d4aa35ad345ae1dbc3eca4
mkl <mkl@sitecore.net>
2016-03-11 13:51:55 +0200

    Languages

---
+++ lang/EN.php
@@ -0,0 +1,39 @@
+<?php
+class LngENG {
+	
+	public $Services = "Services";
+	public $Portfolio = "Portfolio";
+	public $About = "About";
+	public $Team = "Team";
+	public $Contact = "Contact";
+	public $MainTitle = "121 Real Estate Agency";
+	
+	public $WelcomeToOurStudio = "Welcome To Our Studio!";
+	public $ItsNiceToMeetYou = "It's Nice To Meet You";
+	public $TellMeMore = "Tell Me More";
+	
+	public $ServicesDescription = "Lorem ipsum dolor sit amet consectetur";
+	public $Commerce = "Commerce";
+	public $Advertisment = "Advertisment";
+	public $Security = "Security";
+	
+	public $OurAmazingTeam = "Our Amazing Team";
+	
+	public $ContactUs = "Contact Us";
+	
+	public $YourName = "your name";
+	public $YourEmail = "your email";
+	public $YourPhone = "your phone";
+	public $YourMessage = "your message";
+	public $PleaseEnter = "Please enter";
+	public $SendMessage = "Send Message";
+	
+	public $Copyright = "Copyright &copy; 121 Real Estate";
+	
+	
+	/* public $ = ""; */
+	
+};
+
+$_L = new LngENG();
+?>
---
+++ lang/UA.php
@@ -0,0 +1,35 @@
+<?php
+
+class LangUkr {
+	public $Services = "Сервіси";
+	public $Portfolio = "Портфоліо";
+	public $About = "Про нас";
+	public $Team = "Команда";
+	public $Contact = "Контакти";
+	public $MainTitle = "Агенція нерухомості 121 Real Estate";
+	
+	public $WelcomeToOurStudio = "Ласкаво просимо до нашого агенства!";
+	public $ItsNiceToMeetYou = "Приємно познайомитися";
+	public $TellMeMore = "Розкажіть мені більше";
+	
+	public $ServicesDescription = "Наша Команда фахівців завжди надасть Вам швидку та кваліфіковану допомогу";
+	public $Commerce = "Продаж";
+	public $Advertisment = "Реклама";
+	public $Security = "Супровід";
+	
+	public $OurAmazingTeam = 'Наша Чарівна Команда';
+	
+	public $ContactUs = "Зв'язатись із нами";
+	
+	public $YourName = "ваше ім'я";
+	public $YourEmail = "ваш email";
+	public $YourPhone = "ваш телефон";
+	public $YourMessage = "ваше повідомлення";
+	public $PleaseEnter = "Будь ласка введіть";
+	public $SendMessage = "Надіслати";
+	
+	public $Copyright = "Copyright &copy; 121 Real Estate";	
+}
+
+$_L = new LangUkr;
+?>
