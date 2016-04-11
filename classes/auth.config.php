29a7315299890a658101bdc76dc5d34ec264183a
mkl <mkl@sitecore.net>
2016-04-11 15:53:24 +0300

    Admin page auth changed.

--- classes/auth.config.php
+++ classes/auth.config.php
@@ -1,25 +1,40 @@
 <?php
-require __DIR__ . "/class.logsys.php";
-\Fr\LS::config(array(
-    "db" => array(
-      "host" => "localhost",
-      "port" => 3306,
-      "username" => "forcezine_mercury",
-      "password" => "xsZ9d6GX",
-      "name" => "forcezine_mercury",
-      "table" => "main.logins",
-      "token_table" => "main.reset_tokens"
-    ),
-  "features" => array(
-    "auto_init" => true
-  ),
-  "pages" => array(
-    "no_login" => array(
-      "/auth/",
-      "/auth/reset.php",
-      "/auth/register.php"
-    ),
-    "login_page" => "/auth/login.php",
-    "home_page" => "/"
-  )
-));
+require __DIR__ . "/uploader.config.php";
+
+function  AuthoriseThisSession()
+	{
+		session_start();
+		
+
+		if(isset($_SESSION['ee_user_name'], $_SESSION['ee_user_pass']))
+		{
+
+			$userPass = $_SESSION['ee_user_pass'];
+			$userName = $_SESSION['ee_user_name'];
+				
+			if (isset($_CONFIG['users']) && is_array($_CONFIG['users']))
+			{
+				foreach($_CONFIG['users'] as $user)
+				{
+					if($user[1] == $userPass)
+					{
+						echo $user[1] . ' ' . $user[0];
+						
+						if(strlen($userName) == 0 || (strtolower($userName) == strtolower($user[0])))
+						{
+							return true;
+						}
+					}
+				}
+				return false;
+			}
+
+		}
+	return false;	
+	}
+
+if (!AuthoriseThisSession()) header( "Location: /");
+	
+	
+?>
+
---
+++ classes/maestro.config.php
@@ -0,0 +1,15 @@
+<?php
+require_once __DIR__ . '/auth.config.php';
+function GetGlobalConnectionOptions()
+{
+    return array(
+  'server' => 'localhost',
+  'port' => '5432',
+  'username' => 'amuletgrey_estate',
+  'password' => 'hFPaqa2U',
+  'database' => 'amuletgrey_estate'
+);
+}
+
+
+?>
